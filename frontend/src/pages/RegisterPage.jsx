c3e588a6c306f89a33f79f132a65ace84164adbb
user name <7rancissk0@gmail.com>
2025-02-07 16:18:55 -0500

    tailwind css setup fixed

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,16 @@
 function RegisterPage() {
-  return <h3 className="text-2xl font-bold">RegisterPage</h3>;
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">RegisterPage</h3>
+      <form>
+        <input type="text" placeholder="Enter your fullname" />
+        <input type="email" placeholder="Enter your email" />
+        <input type="password" placeholder="Enter your password" />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
+
 
 export default RegisterPage;
