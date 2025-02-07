85fcca4a03d639fe6fde4950ff23acc6da7264ec
user name <7rancissk0@gmail.com>
2025-02-06 23:35:01 -0500

    JSON Web Token creado para signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { pool } from "../db.js";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const signin = (req, res) => {
   res.send("ingresando");
@@ -9,22 +10,24 @@ export const signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-    console.log(hashedPassword);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
       "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
       [name, email, hashedPassword]
     );
 
-    console.log(result);
-
-    return res.json(result.rows[0]);
+    const token = await createAccessToken({ id: result.rows[0].id });
+    
+    // return res.json(result.rows[0]);
+    return res.json({
+      token: token,
+    })
   } catch (error) {
     if (error.code == "23505") {
-      return res.status(400).json({ 
-        message: "El correo ya está registrado", });
+      return res.status(400).json({
+        message: "El correo ya está registrado",
+      });
     }
   }
 }; //* HECHO
