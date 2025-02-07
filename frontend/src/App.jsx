1fd8c88c8186a45ab9e06fc33b8654a1e68df9a9
user name <7rancissk0@gmail.com>
2025-02-07 13:03:04 -0500

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  );
+}
+
+export default App;
