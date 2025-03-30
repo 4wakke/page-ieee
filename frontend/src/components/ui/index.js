ce08b0453cfe97e541036ab2f0a31bee7c9bbb5b
user name <7rancissk0@gmail.com>
2025-03-29 23:45:21 -0500

    prueba register up

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -3,3 +3,7 @@ export { Card } from "./Card";
 export { Input } from "./Input";
 export { Label } from "./Label";
 export { Container } from "./Container";
+export { CardReg } from "./CardReg";
+export { SelectReg } from "./SelectReg";
+
+
