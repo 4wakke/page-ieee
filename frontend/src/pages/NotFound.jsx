31f835c7e1f7a1a079e4b5954f532e752407f2e4
user name <7rancissk0@gmail.com>
2025-03-30 07:05:34 -0500

    Inicio nuevos cambios

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -14,11 +14,11 @@ function NotFound() {
     <div className="w-screen h-screen flex items-center justify-center">
   <div className="bg-[#2e5ca6] bg-opacity-85 w-full max-w-[450px] h-auto p-8 rounded-2xl shadow-2xl flex flex-col items-center justify-center text-center mb-16">
     <h1 className="text-5xl font-extrabold text-white mb-4">Página no encontrada</h1>
-    <h3 className="text-xl text-white mb-4">Error 404</h3>
-    <p className="text-white text-lg mb-6">Para volver al inicio, presiona el botón:</p>
+    <h3 className="text-3xl text-white mb-4 font-bold">Error 404</h3>
+    <p className="text-white text-lg mb-6 font-medium">Para volver al inicio, presiona el botón:</p>
     <Link
       to="/"
-      className="bg-white text-[#0073AE] font-medium py-2 px-6 rounded-lg  hover:bg-[#0073ae] text-lg mb-4 hover:text-[#fff]"
+      className="bg-[#ffffff] hover:bg-[#0073ae] text-[#0073ae] px-4 py-2 rounded font-semibold hover:text-[#fff] tracking-wide duration-300 shadow-md hover:shadow-lg"
     >
       Inicio
     </Link>
