4211f73c7cea02b16d3fdc022ca1ae2fd052e59e
Kevingue10 <Kevinguegra@gmail.com>
2025-04-01 17:56:54 -0500

    change vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: 80,
-  host: "0.0.0.0",
+
   server: {
     allowedHosts: ["ieeecolcaribeconference.com"],
+    port: 80,
+    host: "0.0.0.0",
   },
 });
