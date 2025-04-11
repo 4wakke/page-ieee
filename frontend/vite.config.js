f9bb594b4af319ccf7ef1c07d185846a0ef18145
Kevingue10 <Kevinguegra@gmail.com>
2025-04-10 22:21:56 -0500

    change port in front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: 80,
+  port: 5200,
   host: "0.0.0.0",
   server: {
     allowedHosts: ["ieeecolcaribeconference.com"],
