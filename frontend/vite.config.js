6bc5c2ee4e477c18392188b950d3d31e1383f1e1
Kevingue10 <Kevinguegra@gmail.com>
2025-08-18 21:58:25 -0500

    Change allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["ieeecolcaribeconference.com","temscon.ieeecolcaribeconference.com"],
+    allowedHosts: ["ieeecolcaribeconference.com","c3.ieeecolcaribeconference.com"],
     port: 5200,
     host: "0.0.0.0",
   },
